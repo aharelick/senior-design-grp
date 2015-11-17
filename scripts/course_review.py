245c22b4cb6b8a4674bf8f07dfa40792bae47ed7
Corey Loman <loman@seas.upenn.edu>
2015-11-17 02:31:51 -0500

    removed scripts folder

--- scripts/course_review.py
+++
@@ -1,8 +0,0 @@
-import urllib3
-import bs4
-#contents = urllib3.urlopen("http://api.penncoursereview.com/v1/coursehistories/CIS-120?token=public").read()
-#print contents
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://api.penncoursereview.com/v1/coursehistories/CIS-120?token=public')
-soup = BeautifulSoup(r.read());
-print soup
