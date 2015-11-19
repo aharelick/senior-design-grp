422f2b2e3d7d1a83cd282fde67ecaf628b8c84b2
Alex Harelick <alexharelick@gmail.com>
2015-11-18 23:51:10 -0500

    changed all 4 spaces to 2

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 /* GET dashboard. */
 router.get('/dashboard', function(req, res, next) {
-    res.render('dashboard', { title: 'RaaS'});
+  res.render('dashboard', { title: 'RaaS'});
 });
 
 /* POST signup. */
@@ -19,44 +19,44 @@ router.post('/signup', function(req, res, next) {
 	var email = req.body.email;
 	var password = req.body.password;
 	var confirm = req.body.confirm;
-    req.assert('email', 'Email is not valid').isEmail();
-    req.assert('password', 'Password must be at least 6 characters long').len(6);
-    req.assert('confirm', 'Passwords do not match').equals(req.body.password);
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password must be at least 6 characters long').len(6);
+  req.assert('confirm', 'Passwords do not match').equals(req.body.password);
 
-    var errors = req.validationErrors();
+  var errors = req.validationErrors();
 
-    if (errors) {
-        req.flash('errors', errors);
-        return res.redirect('/');
-    }
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/');
+  }
 
-    var user = new User({
-        email: req.body.email,
-        password: req.body.password,
-    });
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
 
     // email.toLowerCase()?
-    User.findOne({ email: req.body.email }, function(err, existingUser) {
-        if (existingUser) {
-            req.flash('errors', { msg: 'Account with that email address already exists.' });
-            return res.redirect('/');
-        }
-        user.save(function(err) {
-            if (err) return next(err);
-            req.logIn(user, function(err) {
-                if (err) return next(err);
-                res.redirect('/dashboard');
-            });
-        });
+  User.findOne({ email: req.body.email }, function(err, existingUser) {
+    if (existingUser) {
+      req.flash('errors', { msg: 'Account with that email address already exists.' });
+      return res.redirect('/');
+    }
+    user.save(function(err) {
+      if (err) return next(err);
+      req.logIn(user, function(err) {
+        if (err) return next(err);
+        res.redirect('/dashboard');
+      });
     });
+  });
 });
 
 /* POST login. */
 router.post('/login', passport.authenticate('local', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/',
-        failureFlash: true
-    })
+    successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash: true
+  })
 );
 
 router.get('/logout', function(req, res) {
